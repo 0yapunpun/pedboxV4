674555dbd9a5996a5f0202e51be5fb80fed5ff99
dev11 <dev11@dev11s-MacBook-Pro.local>
2022-05-02 14:19:51 -0500

    cambio de variables globales de produccion

--- engine/config.js
+++ engine/config.js
@@ -1,6 +1,6 @@
 const config = {
-  // urlBase: '/root/www/avinal/',
-  urlBase: '/Users/dev11/Desktop/pedboxv4',
+  urlBase: '/root/www/pedboxV.4/pedboxv4',
+  // urlBase: '/Users/dev11/Desktop/pedboxv4',
 };
 
 module.exports = config;
